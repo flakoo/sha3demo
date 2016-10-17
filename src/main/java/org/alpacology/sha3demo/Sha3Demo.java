c77a69df64e18d19d1c497da1d3bc5e14c897988
Jan Falkowski <falkowski.jj@gmail.com>
2016-10-17 22:25:12 +0200

    Corrected byte counting, taking alphabet into account

--- src/main/java/org/alpacology/sha3demo/Sha3Demo.java
+++ src/main/java/org/alpacology/sha3demo/Sha3Demo.java
@@ -4,6 +4,7 @@ import org.bouncycastle.crypto.digests.SHAKEDigest;
 
 public class Sha3Demo {
     public static final String INPUT ="asDge123FtEBlahNeedsMoreL3tter5";
+    public static final String ALPHABET = "AaBbCcDdEeFfHhKkMmOoPpRrTtXxYy34689";
     public static final int OUTPUT_STRING_LENGTH = 7;
 
 
@@ -22,6 +23,25 @@ public class Sha3Demo {
     }
 
     public static int getByteLength() {
-       return OUTPUT_STRING_LENGTH;
+        int length = 0;
+        do {
+            length++;
+        } while(getTotalNumberOfCombinationsForByteLength(length) < getTotalNumberOfHashCombinationsForAlphabet());
+
+        return --length;
+    }
+
+    private static long getTotalNumberOfCombinationsForByteLength(int length) {
+        return Math.round(Math.pow(
+                Double.valueOf(256),
+                Double.valueOf(length)
+        ));
+    }
+
+    private static long getTotalNumberOfHashCombinationsForAlphabet() {
+        return Math.round(Math.pow(
+                Double.valueOf(ALPHABET.length()),
+                Double.valueOf(OUTPUT_STRING_LENGTH)
+        ));
     }
 }
