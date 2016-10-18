9718ba90eb9cd2daf2c3bebe13726d85c448d6f1
Jan Falkowski <falkowski.jj@gmail.com>
2016-10-18 20:00:18 +0200

    Hash bytes to string translation
    Conflicts:
    	src/main/java/org/alpacology/sha3demo/Sha3Demo.java
    
    Hash bytes to string translation

--- src/main/java/org/alpacology/sha3demo/Sha3Demo.java
+++ src/main/java/org/alpacology/sha3demo/Sha3Demo.java
@@ -2,6 +2,10 @@ package org.alpacology.sha3demo;
 
 import org.bouncycastle.crypto.digests.SHAKEDigest;
 
+import java.math.BigInteger;
+import java.util.Stack;
+import java.util.stream.Collectors;
+
 public class Sha3Demo {
     public static final String INPUT ="asDge123FtEBlahNeedsMoreL3tter5";
     public static final String ALPHABET = "AaBbCcDdEeFfHhKkMmOoPpRrTtXxYy34689";
@@ -19,7 +23,30 @@ public class Sha3Demo {
     }
 
     private static String getOutputBytesAsString(byte[] bytes) {
-        return new String(bytes);
+        BigInteger number = new BigInteger(1, bytes);
+
+        Stack<Integer> numberWithCorrectRadix = convertNumberToRadix(number, ALPHABET.length());
+
+        return stackToAlphabet(numberWithCorrectRadix);
+    }
+
+    private static String stackToAlphabet(Stack<Integer> numberWithCorrectRadix) {
+        return numberWithCorrectRadix.stream()
+                .map(digit -> ALPHABET.substring(digit, digit + 1))
+                .collect(Collectors.joining());
+    }
+
+    private static Stack<Integer> convertNumberToRadix(BigInteger number, int radix) {
+        Stack<Integer> stack = new Stack<>();
+        do {
+            BigInteger[] result = number.divideAndRemainder(
+                    BigInteger.valueOf(radix)
+            );
+            stack.push(result[1].intValue()); // remainder
+            number = result[0]; // division result
+        } while (!number.equals(BigInteger.ZERO));
+
+        return stack;
     }
 
     public static int getByteLength() {
